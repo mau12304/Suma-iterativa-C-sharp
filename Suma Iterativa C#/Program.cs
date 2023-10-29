91086ee3ed7de4f43614927cea331a502dbb8e5d
Amin070204 <145484915+Amin070204@users.noreply.github.com>
2023-10-29 09:46:38 -0600

    Agregar archivos de proyecto.

---
+++ Suma Iterativa C#/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static int SumaIterativa(int n)
+    {
+        int suma = 0;
+        while (n > 9)
+        {
+            suma += n % 10;
+            n /= 10;
+        }
+        return suma + n;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Ingresa un número entero: ");
+        if (int.TryParse(Console.ReadLine(), out int n))
+        {
+            int resultado = SumaIterativa(n);
+            Console.WriteLine($"La suma de los dígitos de {n} es: {resultado}");
+        }
+        else
+        {
+            Console.WriteLine("Entrada no válida. Debes ingresar un número entero.");
+        }
+    }
+}
